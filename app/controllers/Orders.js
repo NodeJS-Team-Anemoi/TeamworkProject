f35f62b472cf3b365ef327bb57ae130ac008c6e6
epitsin <epitsin@yahoo.com>
2014-10-07 21:04:12 +0300

    Added Orders Controller.

--- app/controllers/Orders.js
+++ app/controllers/Orders.js
@@ -2,18 +2,78 @@ var Order = require('./../models/Order');
 
 module.exports = {
     getAll: function (req, res) {
-        
+        Order.find({ }, function (error, orders) {
+            if (error) {
+                res.send(error);
+            }
+
+            res.json(orders);
+        });
     },
     getById: function (req, res) {
-        
+        Order.findById(req.params.id, function (error, orders) {
+            if (error) {
+                res.send(error);
+            }
+
+            res.json(orders);
+        });
     },
     create: function (req, res) {
-        
+        var order = new Order({
+            userId: req.body.title,
+            userName: req.body.userName,
+            products: req.body.products.split(', '),
+            date: req.body.date,
+            shippingAddress: req.body.shippingAddress,
+            paymentMethod: req.body.paymentMethod
+        });
+
+        order.save(function (error) {
+            if(error){
+                res.send(error);
+            }
+        });
+
+        res.end();
     },
     update: function (req, res) {
-        
+        Order.findById(req.params.id, function (error, order) {
+            if (error) {
+                res.send(error);
+            }
+
+            order.userId = req.body.title;
+            order.userName =  req.body.userName;
+            order.products =  req.body.products.split(', ');
+            order.date = req.body.date;
+            order.shippingAddress =  req.body.shippingAddress;
+            order.paymentMethod =  req.body.paymentMethod;
+
+            order.save(function (error) {
+                if(error){
+                    res.send(error);
+                }
+            });
+
+            res.end();
+        });
     },
     delete: function (req, res) {
-        
+        Order.findById(req.params.id, function (error, order) {
+            if (error) {
+                res.send(error);
+            }
+
+            order.local.deleted = true;
+
+            order.save(function (error) {
+                if(error){
+                    res.send(error);
+                }
+            });
+
+            res.end();
+        });
     }
 }
