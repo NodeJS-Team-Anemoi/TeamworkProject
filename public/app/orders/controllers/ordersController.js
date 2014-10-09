df63ed785c890319e0f9f011374bf39a839d34bd
vdimitrov94 <vidimitrov94@gmail.com>
2014-10-09 23:47:01 +0300

    Catalog module added. Orders fixes.

--- public/app/orders/controllers/ordersController.js
+++ public/app/orders/controllers/ordersController.js
@@ -1,13 +1,6 @@
 'use strict';
 
-app.controller('OrdersController', ['$scope', '$location', 'auth', 'bookResource',
-    function AddBookController($scope, $location, auth, bookResource) {
-////        if (auth.isAuthorizedForRole('Admin')) {
-//        $scope.createBook = function (book) {
-//            bookResource.create(book)
-//                .then(function () {
-//                    $location.path('/admin');
-//                });
-//        };
-////        }
+app.controller('OrdersController', ['$scope', '$location', 'auth', 'ordersResource',
+    function OrdersController($scope, $location, auth, ordersResource) {
+        
     }]);
